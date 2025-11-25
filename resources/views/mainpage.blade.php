456f83978e238c76479b4677c6159738fef492bc
Klaudia Gaw <klaudia030303@gmail.com>
2025-11-26 00:07:30 +0100

    Zmiana koloru na rozowy

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Wybierz Quiz</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Rozwiąż Quiz</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
 </head>
 <body>
 <div class="container">
